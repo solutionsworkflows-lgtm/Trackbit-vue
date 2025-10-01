73a08b210559bb444b03bfead74ac71e35733602
Kawan Vitor <kawanfreitas108@gmail.com>
2025-10-01 18:01:14 -0300

    remove domains from vite confi

--- vite.config.ts
+++ vite.config.ts
@@ -32,11 +32,11 @@ export default defineConfig({
   ],
   server: {
     port: 3000,
-    https: {
-      key: fs.readFileSync(path.resolve(__dirname, 'app-dev.track.com.br-key.pem')),
-      cert: fs.readFileSync(path.resolve(__dirname, 'app-dev.track.com.br.pem'))
-    },
-    host: 'app-dev.track.com.br',
+    // https: {
+    //   key: fs.readFileSync(path.resolve(__dirname, 'app-dev.track.com.br-key.pem')),
+    //   cert: fs.readFileSync(path.resolve(__dirname, 'app-dev.track.com.br.pem'))
+    // },
+    // host: 'app-dev.track.com.br',
     // hmr: {
     //   host: 'app-dev.wetrackr.com.br',
     //   protocol: "wss",
